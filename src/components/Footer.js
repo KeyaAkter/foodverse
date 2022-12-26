426b8bc14f65168b067ff45a7547fa8800c60baf
KeyaAkter <sanhatanvin12@gmail.com>
2022-12-26 20:20:42 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-8 flex flex-col gap-3 items-center bg-rose-200 opacity-75">
+      <h2 className="text-2xl font-bold lowercase italic">
+        Food<span className="text-rose-500">verse</span>
+      </h2>
+      <p>&copy; {new Date().getFullYear()} Foodverse. All rights reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;
